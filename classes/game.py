f47f23f4db1a96d4e6a917a7ab79be074eb7f70c
Michal Kowalski <kmjk15@gmail.com>
2020-03-08 14:40:25 +0100

    game settings

---
+++ classes/game.py
@@ -0,0 +1,28 @@
+import random
+
+class Bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxHp = hp
+        self.hp = hp
+        self.maxMp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions = ['Attack', 'Magic']
+    
+    def generateDamage(self):
+        return random.randrange(self.atkl, self.atkh)
+    
+    
