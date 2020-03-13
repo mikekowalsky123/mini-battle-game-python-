ef97dfaf383628bc7647e602ba7e7984f0c891e1
Kowalski <Michal.Kowalski@BancTec.com>
2020-03-13 12:25:15 +0100

    More and more learning...

--- classes/game.py
+++ classes/game.py
@@ -25,4 +25,50 @@ class Person:
     def generateDamage(self):
         return random.randrange(self.atkl, self.atkh)
     
+    def generateSpellDamage(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
+    def takeDamage(self, dmg):
+        self.hp -= dmg
+        if(self.hp < 0):
+            self.hp = 0
+        return self.hp
+    
+    def getHp(self):
+        return self.hp
+    
+    def getMaxHp(self):
+        return self.maxHp
+    
+    def getMp(self):
+        return self.mp
+    
+    def getMaxMp(self):
+        return self.maxMp
+    
+    def reduceMp(self, cost):
+        self.mp -= cost
+    
+    def getSpellName(self, i):
+        return self.magic[i]["name"]
+    
+    def getSpellMpCost(self, i):
+        return self.magic[i]["cost"]
+
+    def chooseAction(self):
+        i = 1
+        print("Actions")
+        for item in self.actions:
+            print(str(i) + ":", item)
+            i += 1
+    
+    def chooseMagic(self):
+        print("Magic")
+        i = 1
+        for spell in self.magic:
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) + ")")
+            i += 1
+    
     
